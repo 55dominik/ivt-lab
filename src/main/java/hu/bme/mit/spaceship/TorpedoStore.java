d5d1366600fe65646e6df4465ed19c363c697603
55dominik <burgessdominik@gmail.com>
2020-05-04 15:07:00 +0200

    TorpedoStore errors fixed for codereview branch.

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,6 +1,10 @@
 package hu.bme.mit.spaceship;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Random;
+
+import javax.management.RuntimeErrorException;
 
 /**
 * Class storing and managing the torpedoes of a ship
@@ -11,10 +15,10 @@ public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
-
+  private Random rand; 
   private int torpedoCount = 0;
 
-  public TorpedoStore(int numberOfTorpedos){
+  public TorpedoStore(int numberOfTorpedos) {
     this.torpedoCount = numberOfTorpedos;
 
     // update failure rate if it was specified in an environment variable
@@ -26,22 +30,28 @@ public class TorpedoStore {
         FAILURE_RATE = 0.0;
       }
     }
+
+    try {
+      rand = SecureRandom.getInstanceStrong(); 
+    } catch (NoSuchAlgorithmException e) {
+      rand = new Random();
+    }
   }
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
-    double r = generator.nextDouble();
+    
+    double r = rand.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount =- numberOfTorpedos;
+      this.torpedoCount -= numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
